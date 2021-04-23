397574edbca8f41eb3b3e040d048a17f872d1d94
aminniktash2010 <80291034+aminniktash2010@users.noreply.github.com>
2021-04-23 13:06:21 -0400

    01

--- azwebapp.tf
+++ azwebapp.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 ##############################
 ## Azure App Service - Main ##
 ##############################
